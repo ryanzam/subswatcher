45e5c8876d7057d0b43a237e87629e892647ec2d
ranjam <ryanzam2005@gmail.com>
2025-02-15 17:43:19 +0545

    add middleware

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 import subsRouter from "./routes/subs.route.js";
 import connectDB from "./database/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/subs", subsRouter)
+
+app.use(errorMiddleware)
 
 app.get("/", (req, res) => {
     res.send("This is Sub Watcher API!")
